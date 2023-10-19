daed7154592a0189db02d50e4f42aaa495a32652
gary <henry.gary@hotmail.com>
2023-10-19 20:45:12 +0200

    Add Menu component for the displays of audios and images

---
+++ src/Menus/MenuImage.svelte
@@ -0,0 +1,27 @@
+<script>
+    // Ici je vais fetch tous mes audios
+</script>
+
+<div class="menu__image">
+    <button class="bloc_image">1</button>
+    <button class="bloc_image">2</button>
+    <button class="bloc_image">3</button>
+    <button class="bloc_image">4</button>
+    <button class="bloc_image">5</button>
+    <button class="bloc_image">6</button>
+    <button class="bloc_image">7</button>
+</div>
+
+<style>
+    .bloc_image {
+        width: 240px;
+        aspect-ratio: 1;
+        background-color: hotpink;
+        margin-inline: auto;
+        margin-top: 2rem;
+        font-size: 3rem;
+        display: grid;
+        place-content: center;
+        border: none;
+    }
+</style>
---
+++ src/lib/Menu.svelte
@@ -0,0 +1,29 @@
+<script>
+    // TODO: Faire venir les images du fichier ici
+    console.log("Je suis dans le menu.");
+    import MenuImage from "../Menus/MenuImage.svelte";
+    import MenuAudio from "../Menus/MenuAudio.svelte";
+</script>
+
+<div class="menu" data-visible="false">
+    <MenuImage />
+    <MenuAudio />
+</div>
+
+<style>
+    .menu {
+        background-color: #d9d9d9;
+        width: 300px;
+        position: absolute;
+        top: 0;
+        right: 0;
+        height: 100%;
+        transition: transform 350ms ease-out;
+        overflow-y: scroll;
+    }
+
+    /* TODO: Je ne sais pas faire fonctionner ce truc en fait. */
+    .menu[data-visible="false"] {
+        transform: translateX(100%);
+    }
+</style>
