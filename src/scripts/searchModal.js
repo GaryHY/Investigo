1492d94aeda8fbefb4d417c0d6d0c5fdd811fe6a
gary <henry.gary@hotmail.com>
2023-10-19 20:41:50 +0200

    feat: add open and close SearchBar Modal scripts
    The content of this script was in the file containing the search bar. This
    file was made to improve readability in the search bar componenent.

---
+++ src/scripts/searchModal.js
@@ -0,0 +1,10 @@
+export function openModal() {
+    const modal = document.querySelector(".search__modal");
+    modal.style.display = "block";
+};
+
+export function closeModal(e) {
+    const modal = document.querySelector(".search__modal");
+    modal.style.display = "none";
+};
+
