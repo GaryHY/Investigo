0e1b4232dcb9ee229cb1d06660fa737e51f04c20
gary <henry.gary@hotmail.com>
2023-10-20 13:47:31 +0200

    Change default state of selected element in sidebar
    Je devrais peut etre mettre cela comme un fixup commit pour que je puisse
    autosquash tout cela apres

--- src/stores/sidebar.js
+++ src/stores/sidebar.js
@@ -1,9 +1,10 @@
 // TODO: Use typescript and enum/union types to define the states.
 import {writable} from "svelte/store"
 
-const state = writable("rapport");
+const state = writable("mandat");
+// TODO: Remettre ce qu'il y a en dessous comme valeur initiale
+// const state = writable("rapport");
 
 export default state;
 
 
-// TODO: Make the modal for the photo, envoyer et reseaux part
