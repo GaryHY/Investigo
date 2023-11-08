1a021e4ca49cfdc12dafe0083b49ad4cadd94250
gary <henry.gary@hotmail.com>
2023-11-08 23:46:35 +0100

    feat: create Templates components (some) first versin

---
+++ src/Templates/Rapport.svelte
@@ -0,0 +1,22 @@
+<script>
+    // TODO: Mettre ici le code qui se trouve en ce moment dans le main.
+    export let element;
+</script>
+
+<div class="template">
+    <img
+        class="imgtest"
+        src="http://via.placeholder.com/60?text=1"
+        alt="phototest"
+    />
+    <h2>Un titre pour tester en fait.</h2>
+</div>
+<div class="editortext" bind:this={element} />
+
+<style>
+    .template {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+</style>
