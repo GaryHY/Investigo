043e2cb5fc039de426198a45c6cd297efa9b75bf
gary <henry.gary@hotmail.com>
2023-11-08 23:46:35 +0100

    Add some of the Templates components

---
+++ src/Templates/Devis.svelte
@@ -0,0 +1,8 @@
+<div class="template">
+    <img
+        class="imgtest"
+        src="http://via.placeholder.com/60?text=1"
+        alt="phototest"
+    />
+    <h2>Je suis dans la partie devis en fait.</h2>
+</div>
---
+++ src/Templates/Mandat.svelte
@@ -0,0 +1,41 @@
+<script>
+    // NOTE: Ici on va juste faire des templates cela va etre hyper facile.
+
+    // TODO: Faire en sorte de recuperer les infos du store concernant le dossier.
+    let user = "John Doe";
+
+    // Quand je serai sur la fin du projet.
+    // import Logo from ../Images/Logo
+    // import SNARP from ../Images/SNARP
+</script>
+
+<div class="mandat">
+    <div class="mandat__top flex">
+        <img
+            class="imgtest"
+            src="http://via.placeholder.com/120?text=1"
+            alt="phototest"
+        />
+        <img
+            class="imgtest"
+            src="http://via.placeholder.com/120?text=1"
+            alt="phototest"
+        />
+    </div>
+    <br />
+    <br />
+    <br />
+    <h2 class="tacenter">Mandat</h2>
+    <h3 class="tacenter">Contrat de mission</h3>
+    <p class="italic">
+        Type de contrat preconise par le syndicat National des Agents de
+        Recherche priveees.
+    </p>
+    <p class="underline uppercase bold">Entre les soussignes:</p>
+</div>
+
+<style>
+    .mandat > * {
+        outline: 1px solid red;
+    }
+</style>
