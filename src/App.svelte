3fe144b58dcc865c1080bcc2eb0086c468c311b2
gary <henry.gary@hotmail.com>
2023-11-08 23:39:46 +0100

    Change the app component to add the elements of the layout

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import Header from "./lib/Header.svelte";
+    import Sidebar from "./lib/Sidebar.svelte";
+    import Main from "./lib/Main.svelte";
+
+    // NOTE: I need this info to understand what is need to be displayed in terms of functionanlity and I need it for the
+    // differents request that I am going to make.
+    // let user;
+</script>
+
+<div class="single_page">
+    <Header />
+    <Sidebar />
+    <Main />
+</div>
+
+<style>
+</style>
