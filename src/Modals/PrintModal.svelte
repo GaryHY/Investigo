3daddef061d6f9e4bf80dc9facc9578ee3394c34
gary <henry.gary@hotmail.com>
2023-11-13 21:57:16 +0100

    fix: change Mainbar component styling

---
+++ src/Modals/PrintModal.svelte
@@ -0,0 +1,21 @@
+<div class="previewModal">
+    <h2>Un titre juste pour la forme</h2>
+    <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error
+        consectetur officia excepturi laborum, dolore ullam eveniet quod eum
+        maiores assumenda libero, sint nemo? Libero quod sed velit? Magnam,
+        voluptates et! Eveniet non cumque id odio incidunt soluta, eum animi
+        laborum facilis. Asperiores corrupti praesentium pariatur quaerat rem
+        ducimus earum, corporis eius sed animi voluptas amet aspernatur natus
+        dolorum placeat assumenda culpa molestiae ipsum neque soluta repudiandae
+        sapiente ut maxime architecto. In commodi deserunt recusandae alias iste
+        doloribus similique ullam debitis!
+    </p>
+</div>
+
+<style>
+    .previewModal {
+        border: 2px solid blue;
+        width: 1000px;
+    }
+</style>
--- src/lib/MainBar.svelte
+++ src/lib/MainBar.svelte
@@ -12,6 +12,9 @@
     import TALeft from "../Icons/Mainbar/TALeft.svg";
     import TACenter from "../Icons/Mainbar/TACenter.svg";
     import TARight from "../Icons/Mainbar/TARight.svg";
+    import Print from "../Icons/Mainbar/Print.svg";
+    import Save from "../Icons/Mainbar/Save.svg";
+    import Undo from "../Icons/Mainbar/Undo.svg";
     import Heading1 from "../Icons/Mainbar/Heading-1.svg";
     import Heading2 from "../Icons/Mainbar/Heading-2.svg";
     import Paragraph from "../Icons/Mainbar/Paragraph.svg";
@@ -20,84 +23,35 @@
 
     // Modal
     import Modal from "./Modal.svelte";
-    import PreviewModal from "../Modals/PreviewModal.svelte";
+    import PdfModal from "../Modals/PdfModal.svelte";
+    import PrintModal from "../Modals/PrintModal.svelte";
     import { openModal } from "../scripts/modal";
 
+    function no_function() {
+        console.log("Je dois implementer la fonction");
+    }
 </script>
 
 {#if editor}
     <div class="main_bar">
-        <button
-            class="mainbar__button"
-            on:click={() => editor.chain().focus().toggleBold().run()}
-            class:active={editor.isActive("bold")}
-        >
-            <img src={Bold} alt="bold icon" />
-            <div class="mainbar__tooltip">Bold</div>
-        </button>
-        <button
-            class="mainbar__button"
-            on:click={() => editor.chain().focus().toggleItalic().run()}
-            class:active={editor.isActive("italic")}
-        >
-            <img src={Italic} alt="italic icon" />
-            <div class="mainbar__tooltip">Italic</div>
-        </button>
-        <button
-            class="mainbar__button"
-            on:click={() => editor.chain().focus().toggleUnderline().run()}
-            class:active={editor.isActive("underline")}
-        >
-            <img src={Underline} alt="underline icon" />
-            <div class="mainbar__tooltip">Underline</div>
-        </button>
-        <button
-            class="mainbar__button"
-            on:click={() => editor.chain().focus().setTextAlign("left").run()}
-            class:active={editor.isActive({ textAlign: "left" })}
-        >
-            <img src={TALeft} alt="left align icon" />
-            <div class="mainbar__tooltip">Aligner a gauche</div>
-        </button>
-
-        <button
-            class="mainbar__button"
-            on:click={() => editor.chain().focus().setTextAlign("center").run()}
-            class:active={editor.isActive({ textAlign: "center" })}
-        >
-            <img src={TACenter} alt="center align icon" />
-            <div class="mainbar__tooltip">Aligner au centre</div>
-        </button>
-
-        <button
-            class="mainbar__button"
-            on:click={() => editor.chain().focus().setTextAlign("right").run()}
-            class:active={editor.isActive({ textAlign: "right" })}
-        >
-            <img src={TARight} alt="right align icon" />
-            <div class="mainbar__tooltip">Aligner a droite</div>
-        </button>
-        <Modal>
-            <PreviewModal />
-        </Modal>
-        <button class="mainbar__button open_modal" on:click={openModal}>
-            <img class="icon" src={Pdf} alt="icon pdf" />
-            <div class="mainbar__tooltip">Apercu PDF</div>
-        </button>
-        <button
-            class="mainbar__button open_menu audio_menu"
-            on:click={openMenu}
-        >
-            <img class="icon" src={Audio} alt="icon audio" />
-            <div class="mainbar__tooltip">Enregistrements</div>
-        </button>
-        <button
-            class="mainbar__button open_menu image_menu"
-            on:click={openMenu}
-        >
-            <img class="icon" src={Hamburger} alt="icon hamburger" />
-            <div class="mainbar__tooltip">Apercu image</div>
-        </button>
+        <div class="main_bar_group">
+            <button class="mainbar__button open_modal" on:click={no_function}>
+                <img class="icon" src={Save} alt="icon sauvegarder" />
+                <div class="mainbar__tooltip">Sauvegarder</div>
+            </button>
+            <button class="mainbar__button open_modal" on:click={no_function}>
+                <img class="icon" src={Undo} alt="icon pdf" />
+                <div class="mainbar__tooltip">Undo</div>
+            </button>
+            <Modal>
+                <PrintModal />
+            </Modal>
+            <button class="mainbar__button open_modal" on:click={openModal}>
+                <img class="icon" src={Print} alt="icon print" />
+                <div class="mainbar__tooltip">Imprimer</div>
+            </button>
+        </div>
+        <div class="main_bar_group split">
             <button
                 class="mainbar__button"
                 on:click={() => {
@@ -125,6 +79,83 @@
                 <img class="icon" src={Paragraph} alt="icon print" />
                 <div class="mainbar__tooltip">Parargraphe</div>
             </button>
+            <button
+                class="mainbar__button"
+                on:click={() => editor.chain().focus().toggleBold().run()}
+                class:active={editor.isActive("bold")}
+            >
+                <img src={Bold} alt="bold icon" />
+                <div class="mainbar__tooltip">Bold</div>
+            </button>
+            <button
+                class="mainbar__button"
+                on:click={() => editor.chain().focus().toggleItalic().run()}
+                class:active={editor.isActive("italic")}
+            >
+                <img src={Italic} alt="italic icon" />
+                <div class="mainbar__tooltip">Italic</div>
+            </button>
+            <button
+                class="mainbar__button"
+                on:click={() => editor.chain().focus().toggleUnderline().run()}
+                class:active={editor.isActive("underline")}
+            >
+                <img src={Underline} alt="underline icon" />
+                <div class="mainbar__tooltip">Underline</div>
+            </button>
+            <button
+                class="mainbar__button"
+                on:click={() =>
+                    editor.chain().focus().setTextAlign("left").run()}
+                class:active={editor.isActive({ textAlign: "left" })}
+            >
+                <img src={TALeft} alt="left align icon" />
+                <div class="mainbar__tooltip">Aligner a gauche</div>
+            </button>
+
+            <button
+                class="mainbar__button"
+                on:click={() =>
+                    editor.chain().focus().setTextAlign("center").run()}
+                class:active={editor.isActive({ textAlign: "center" })}
+            >
+                <img src={TACenter} alt="center align icon" />
+                <div class="mainbar__tooltip">Aligner au centre</div>
+            </button>
+
+            <button
+                class="mainbar__button"
+                on:click={() =>
+                    editor.chain().focus().setTextAlign("right").run()}
+                class:active={editor.isActive({ textAlign: "right" })}
+            >
+                <img src={TARight} alt="right align icon" />
+                <div class="mainbar__tooltip">Aligner a droite</div>
+            </button>
+        </div>
+        <div class="main_bar_group mla">
+            <Modal>
+                <PdfModal />
+            </Modal>
+            <button class="mainbar__button open_modal" on:click={openModal}>
+                <img class="icon" src={Pdf} alt="icon pdf" />
+                <div class="mainbar__tooltip">Apercu PDF</div>
+            </button>
+            <button
+                class="mainbar__button open_menu audio_menu"
+                on:click={openMenu}
+            >
+                <img class="icon" src={Audio} alt="icon audio" />
+                <div class="mainbar__tooltip">Enregistrements</div>
+            </button>
+            <button
+                class="mainbar__button open_menu image_menu"
+                on:click={openMenu}
+            >
+                <img class="icon" src={Hamburger} alt="icon hamburger" />
+                <div class="mainbar__tooltip">Apercu image</div>
+            </button>
+        </div>
     </div>
 {/if}
 
@@ -136,8 +167,21 @@
     .main_bar {
         display: flex;
         align-items: center;
+        justify-content: space-between;
         padding-inline: 2rem;
+    }
+
+    .main_bar_group {
+        display: flex;
+        align-items: center;
         gap: 0.5rem;
+        margin-inline: 1rem;
+    }
+
+    .split {
+        border-right: 2px solid white;
+        border-left: 2px solid white;
+        padding-inline: 4rem;
     }
 
     .mainbar__button {
