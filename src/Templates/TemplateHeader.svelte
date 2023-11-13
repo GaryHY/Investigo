c103cb7e32fba696a24ca21fc64ea7c2f0026544
gary <henry.gary@hotmail.com>
2023-11-13 19:05:54 +0100

    feat: add Header component for html templates

---
+++ src/Templates/TemplateHeader.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import logo_snarp from "../Icons/Templates/logo_snarp.png";
+    import logo_air from "../Icons/Templates/logo_air.png";
+    export let title: string;
+</script>
+
+<div class="template">
+    <p class="template__header tacenter bold uppercase">
+        agence a.i.r agence d’investigations & recherches
+    </p>
+    <div class="flex images mt-large">
+        <img class="logo_air" src={logo_air} alt="logo_air" />
+        <img class="logo_snarp" src={logo_snarp} alt="logo_snarp" />
+    </div>
+    <h2 class="title mt-large tacenter">{title}</h2>
+</div>
+
+<style>
+    .template__header {
+        text-transform: uppercase;
+    }
+
+    .title {
+        text-transform: capitalize;
+    }
+
+    .logo_air {
+        width: 15rem;
+    }
+</style>
